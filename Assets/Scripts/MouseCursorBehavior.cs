78d9127dc139cef58173b4f6678280e222ef22cc
ZDesign6 <zd7business@gmail.com>
2026-02-28 14:44:26 -0500

    Mouse Cursor Fully Reactive

--- Assets/Scripts/MouseCursorBehavior.cs
+++ Assets/Scripts/MouseCursorBehavior.cs
@@ -24,57 +24,66 @@ public class MouseCursorBehavior : MonoBehaviour
     {
         mouseTransform.position = gameManager.mouseInWorldSpace;
 
-        //if we aren't currently listening or talking
-        if (gameManager.currentlyListening == false && gameManager.currentlyTyping == false)
-        {
-            // Fires a raycasting ray from the mouse pos every frame and detects collision
-            fireRay();
-        }
-        //else if we are currently listening
-        else if (gameManager.currentlyListening == true)
+        fireRay();
+
+        // -- ANIMATION STATES --
+
+        //if we are currently listening, short circuit to Listening Animation
+        if (gameManager.currentlyListening == true)
         {
             // The mouse cursor plays the listening animation
             mouseAnimator.Play("Listen Animation");
         }
-        //else if we are currently typing
+        //else if we are currently typing, short circuit to typing animation
         else if (gameManager.currentlyTyping == true)
         {
             // The mouse cursor plays the speaking animation
             mouseAnimator.Play("Speak Animation");
         }
+        //else, if we are NOT listening and NOT typing, if we ARE hovering over an interactible object...
+        else if (objectHitList.Count > 0 && objectHitList[0].GetComponent<InteractibilityManager>() != null)
+        {
+            Debug.Log("Mouse cursor's ray collided with " + objectHitList[0].name);
+            //if the object is interactible
+            if (objectHitList[0].GetComponent<InteractibilityManager>().isInteractible == true)
+            {
+                // The mouse cursor plays the interacting animation
+                mouseAnimator.Play("Interact Animation");
+            }
+            //if the object is not interactible
+            else
+            {
+                //mouse cursor plays the not interactible animation
+                mouseAnimator.Play("No Interact Animation");
+            }
+        }
+        //else, we are NOT listening, NOT typing, and NOT hovering. So play Neutral animation.
+        else
+        {
+            // The mouse cursor plays the neutral animation
+            mouseAnimator.Play("Neutral Animation");
+        }
+ 
+        // -- CLEANUP --
+        Cleanup();
     }
     
     // Fires a ray from the mouse and detects collision
     void fireRay()
     {
-        //A boolean that is true if the ray shot from the mouse position is hitting something
-        RaycastHit2D hit = Physics2D.Raycast(mouseTransform.position, -Vector2.up);
+        //Cast a Ray and store data about what it hit
+        RaycastHit2D hit = Physics2D.Raycast(mouseTransform.position, new Vector3(0,0,1));
 
-        // If the ray is hitting something
-        if (hit)
+        // If the ray did indeed hit something
+        if (hit == true)
         {
             // The object that is hit is added to the object hit list
             objectHitList.Add(hit.transform.GameObject());
-
-            // If the object hit has transition on click behavior (interactable object)
-            // **For now detects transition on click but can be changed
-            if (objectHitList[0].GetComponent<MirrorTransitionBehavior>() != null)
-            {
-                // **Insert getting values in here or something
-                // **If interactable does interact animation, else does no interact animation yay
-                
-                // The mouse cursor plays the interacting animation
-                mouseAnimator.Play("Interact Animation");
-            }
-            // If the object being hit is not interactable
-            else
-            {
-                // The mouse cursor plays the neutral animation
-                mouseAnimator.Play("Neutral Animation");
-            }
-            
-            // The object that is hit is then removed from the object hit list
-            objectHitList.Remove(hit.transform.GameObject());
         }
+    }
+    void Cleanup()
+    {
+        //empty the objectHitList
+        objectHitList.Clear();
     }
 }
