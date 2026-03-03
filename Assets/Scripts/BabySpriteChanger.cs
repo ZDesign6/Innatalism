cf2d988d8da8698d49ac715ff0ff438305669d67
CDeRosa <cderosa179@gmail.com>
2026-03-03 12:19:35 -0500

    starting to implement shader on baby - currently not functional

--- Assets/Scripts/BabySpriteChanger.cs
+++ Assets/Scripts/BabySpriteChanger.cs
@@ -10,13 +10,14 @@ public class BabySpriteChanger : MonoBehaviour
     GameManagerBehavior gameManager;
     //a two dimensional List that contains a List of all days, with each Day containing a List of each possible sprite for that day's possible extremisms.
     public Sprite[] babySprites = new Sprite[153];
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //assign ref to the singleton
         gameManager = GameManagerBehavior.singleton;
 
-        //-- SPRITE ASSIGNEMENT --
+        //-- SPRITE ASSIGNMENT --
 
         //convert currentDay into a base Index that represents what day's-worth of indices to check
         int baseIndex = (gameManager.currentDay - 1) * 17;
@@ -26,13 +27,15 @@ public class BabySpriteChanger : MonoBehaviour
         int spriteIndex = baseIndex + offsetIndex;
         //assign the Sprite equal to the Sprite at the spriteIndex
         this.gameObject.GetComponent<SpriteRenderer>().sprite = babySprites[spriteIndex];
+        //assign the texture on the wobble shader to the current sprite
+        this.gameObject.GetComponent<SpriteRenderer>().material.mainTexture =  babySprites[spriteIndex].texture;
         //and set this baby's pos equal to the pos of the background, so the two objects are centered on one another
         this.gameObject.GetComponent<Transform>().position = GameObject.Find("Background").GetComponent<Transform>().position;
         //and set this baby's scale equal to the scale of the Background object
         this.gameObject.GetComponent<Transform>().localScale = GameObject.Find("Background").GetComponent<Transform>().localScale;
         //and then recalculate the PolygonCollider's points to match any new Sprite shapes
         this.gameObject.GetComponent<PolygonCollider2D>().CreateFromSprite(this.gameObject.GetComponent<SpriteRenderer>().sprite);
-
+        
     }
 
     // Update is called once per frame
