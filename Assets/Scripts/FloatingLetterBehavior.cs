ff659280aded0b4ab6e4d3bde8e1d3aa5af03086
ZDesign6 <zd7business@gmail.com>
2026-03-23 15:34:34 -0400

    Started FloatingLetter Animation
    they now move towards their destination positions

--- Assets/Scripts/FloatingLetterBehavior.cs
+++ Assets/Scripts/FloatingLetterBehavior.cs
@@ -12,6 +12,13 @@ public class FloatingLetterBehavior : MonoBehaviour
     //where this letter's destination is
     public Vector3 destinationPos;
 
+    // -- "ANIMATION" --
+
+    //tracks if we have reached our destinationPos. Starts on, turned off when we reach the pos.
+    public bool reachedDestinationPos = false;
+    //controls the minumum distance from destinationPos we can be before we are marked as having "reached" it
+    public float maxPosDeviation = .1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,12 +38,33 @@ public class FloatingLetterBehavior : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //every frame, subtract one from lifeInFrames
-        lifeInFrames = lifeInFrames - 1;
-        //if life in frames ever goes below one, delete self
-        if (lifeInFrames <= 0)
+        //if we have reached our destination
+        if (reachedDestinationPos == true)
         {
-            Destroy(this.gameObject);
+            //every frame, subtract one from lifeInFrames
+            lifeInFrames = lifeInFrames - 1;
+            //if life in frames ever goes below one, delete self
+            if (lifeInFrames <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        //if we have not yet reached our destination
+        else
+        {
+            //move towards destination pos
+            this.gameObject.GetComponent<RectTransform>().position = Vector3.MoveTowards(this.gameObject.GetComponent<RectTransform>().position, destinationPos, .1f);
+            //abstract the maxAcceptablePos
+            Vector3 maxAcceptablePos = new Vector3(destinationPos.x + maxPosDeviation, destinationPos.y + maxPosDeviation, destinationPos.z + maxPosDeviation);
+            //abstract the minAcceptablePos
+            Vector3 minAcceptablePos = new Vector3(destinationPos.x - maxPosDeviation, destinationPos.y -  maxPosDeviation, destinationPos.z - maxPosDeviation);
+            //and if we arrive within the acceptable deviation of destinationPos, turn on reachedDestinationPos
+            if (this.gameObject.GetComponent<RectTransform>().position.x <= maxAcceptablePos.x && this.gameObject.GetComponent<RectTransform>().position.x >= minAcceptablePos.x 
+                && this.gameObject.GetComponent<RectTransform>().position.y <= maxAcceptablePos.y && this.gameObject.GetComponent<RectTransform>().position.y >= minAcceptablePos.y
+                && this.gameObject.GetComponent<RectTransform>().position.z <= maxAcceptablePos.z && this.gameObject.GetComponent<RectTransform>().position.z >= minAcceptablePos.z)
+            {
+                reachedDestinationPos = true;
+            }
         }
     }
 }
