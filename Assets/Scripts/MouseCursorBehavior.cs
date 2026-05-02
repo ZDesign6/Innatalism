81b1cf70813f67c690f38431e4b29ea4c950aa1b
CDeRosa <cderosa179@gmail.com>
2026-05-02 13:28:10 -0400

    Mouse no longer renders during Squish in Neutral Ending

--- Assets/Scripts/MouseCursorBehavior.cs
+++ Assets/Scripts/MouseCursorBehavior.cs
@@ -35,6 +35,44 @@ public class MouseCursorBehavior : MonoBehaviour
             {
                 this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
             }
+            else
+            {
+                //if we are currently listening, short circuit to Listening Animation
+                if (gameManager.currentlyListening == true)
+                {
+                    // The mouse cursor plays the listening animation
+                    mouseAnimator.Play("Listen Animation");
+                }
+                //else if we are currently typing, short circuit to typing animation
+                else if (gameManager.currentlyTyping == true)
+                {
+                    // The mouse cursor plays the speaking animation
+                    mouseAnimator.Play("Speak Animation");
+                }
+                //else, if we are NOT listening and NOT typing, and we ARE hovering over an interactible object...
+                else if (objectHitList.Count > 0 && objectHitList[0].GetComponent<InteractibilityManager>() != null)
+                {
+                    //If the object in question has a hover manager, play its hover animation
+                    if (objectHitList[0].GetComponent<HoverManager>() != null)
+                    {
+                        var hoverManager = objectHitList[0].GetComponent<HoverManager>();
+                        hoverManager.AnimateHover();
+                    }
+
+                    //if the object is interactible
+                    if (objectHitList[0].GetComponent<InteractibilityManager>().isInteractible == true)
+                    {
+                        // The mouse cursor plays the interacting animation
+                        mouseAnimator.Play("Interact Animation");
+                    }
+                    //if the object is not interactible
+                    else
+                    {
+                        //mouse cursor plays the not interactible animation
+                        mouseAnimator.Play("No Interact Animation");
+                    }
+                }
+            }
         }
         else
         {
